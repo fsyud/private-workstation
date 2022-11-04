707e4125b4471e173fbfd115a8c43f912391c350
ligdy <dy179912099@163.com>
2022-11-04 17:43:41 +0800

    feat: color add

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,4 +1,5 @@
-import logo from "@assets/react.svg";
+import logo from "@assets/search.svg";
+import github from "@assets/github.svg";
 import "@pages/popup/Popup.css";
 
 const Popup = () => {
@@ -6,17 +7,19 @@ const Popup = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+
+        <input className="app-input" type="text" />
+
+        <div className="app-footer">
+          <a
+            className="App-link"
+            href="https://github.com/ligdy7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={github} alt="" />
+          </a>
+        </div>
       </header>
     </div>
   );
