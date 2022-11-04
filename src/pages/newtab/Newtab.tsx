8895ff506770a27c05ba415715a06cd0c1db6050
ligdy <dy179912099@163.com>
2022-11-04 16:58:48 +0800

    feat: color add

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -3,9 +3,22 @@ import "./Newtab.css";
 const Newtab = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h6>The color of this paragraph is defined using SASS.</h6>
-        <h4>Charles Dong</h4>
+      <div className="background">
+        <div
+          className="background-item"
+          style={{
+            backgroundImage:
+              'url("https://user-images.githubusercontent.com/26371465/199908190-06a7d076-0062-4cc8-88be-b568717a82f0.jpg")',
+          }}
+        ></div>
+      </div>
+      <header className="apps">
+        <div className="intro-middle-wrapper">
+          <div className="prompt">
+            <h1>Lig dyu you are great today</h1>
+            <h3>So try your best today</h3>
+          </div>
+        </div>
       </header>
     </div>
   );
