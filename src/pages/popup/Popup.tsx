a8db22c82ef2d71b039958aedf27c4b3349410eb
ligdy <dy179912099@163.com>
2022-11-08 14:43:56 +0800

    feat: rss change

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,26 +1,26 @@
 import logo from "@assets/search.svg";
-import github from "@assets/github.svg";
 import "@pages/popup/Popup.css";
 
 const Popup = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <input className="app-input" type="text" />
-
-        <div className="app-footer">
-          <a
-            className="App-link"
-            href="https://github.com/ligdy7"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={github} alt="" />
-          </a>
-        </div>
-      </header>
+    <div className="App app-light">
+      <div className="app-main">
+        <header className="app-header">
+          <div className="app-logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <input className="app-input" type="text" />
+        </header>
+        <main>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+          </ul>
+        </main>
+        <footer className="app-footer"></footer>
+      </div>
     </div>
   );
 };
