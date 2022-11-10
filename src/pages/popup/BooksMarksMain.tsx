ce0bee004b53d7b24458160afaba71497f5f41db
ligdy <dy179912099@163.com>
2022-11-10 16:30:45 +0800

    feat: add context

---
+++ src/pages/popup/BooksMarksMain.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect, useContext } from "react";
+import "@pages/popup/BooksMarksMain.css";
+import { AppContext } from "./index";
+import getBookMarks, { Bookmark } from "@utils/tools/book-marks";
+
+const BooksMarksMain: React.FC<{}> = () => {
+  const [bookList, setBookList] = useState<Bookmark[]>([]);
+
+  useEffect(() => {
+    getFocusBookMarks();
+  }, []);
+
+  const getFocusBookMarks = async () => {
+    const BOOKMARKS = await getBookMarks();
+    console.log(BOOKMARKS, "BOOKMARKS");
+    setBookList(BOOKMARKS.slice(0, 100));
+  };
+
+  const value = useContext(AppContext);
+
+  console.log(value, "value");
+
+  return (
+    <>
+      {bookList.length > 0 && (
+        <main className="bookmarks-list">
+          <ul>
+            {bookList.map((item: Bookmark, index: number) => {
+              return (
+                <li key={index}>
+                  <img src={`chrome://favicon/${item.url}`} />
+                  <a>{item.title}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </main>
+      )}
+    </>
+  );
+};
+
+export default BooksMarksMain;
