93814d7b9bbc69fd3e78ffc2d811f194093d0d54
ligdy <dy179912099@163.com>
2022-11-10 17:44:49 +0800

    feat: add context

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,36 +1,16 @@
-import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import Popup from "./Popup";
-import BooksMarksMain from "./BooksMarksMain";
+import MainApp from "./main-app";
+
 import "@pages/popup/index.css";
 
-// 声明Context的数据模型
-interface CountProps {
-  inputVal?: string;
-  setInputVal?: (e: any) => void;
-}
-
-export const AppContext = React.createContext({} as CountProps);
-
-function pageInit() {
-  const [inputVal, setInputValInit] = useState<string>();
-
-  const AppPopup = document.querySelector("body");
-
-
-  const setInputVal = (val: any) => {
-    setInputValInit(val)
-  }
+const pageInit = () => {
+  const AppPopup = document.querySelector("#popup-app");
 
   if (!AppPopup) {
     throw new Error("Can not find AppPopup");
   }
 
-  ReactDOM.createRoot(AppPopup).render(
-    <AppContext.Provider value={{ inputVal, setInputVal }}>
-      <Popup /> <BooksMarksMain />
-    </AppContext.Provider>
-  );
-}
+  ReactDOM.createRoot(AppPopup).render(<MainApp />);
+};
 
 pageInit();
